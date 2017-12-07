76366bf0d4625e17cdc3daa947b9f151b15d2a58
Jatin Luthra <jatinluthra14@gmail.com>
2017-12-07 17:01:15 +0530

    5 JUnit Test Examples

---
+++ src/test/java/org/jbossoutreach/JunitExample.java
@@ -0,0 +1,41 @@
+package org.jbossoutreach;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+
+
+public class JunitExample {
+    @Test
+    public void assert_True()
+    {
+        int s = 1 + 2;         
+        Assert.assertTrue(s == 3);
+    }
+    @Test
+    public void assert_False()
+    {
+        int s = 3 - 2;
+        Assert.assertFalse(s == 0);
+    }
+    @Test
+    public void assert_Equals()
+    {
+        int d = 5 / 5;
+        Assert.assertEquals(d, 1);
+    }
+    @Test
+    public void assert_Array_Equals()
+    {
+        int a1[]={1,2,3};
+        int a2[]={1,2,3};
+        Assert.assertArrayEquals(a1 ,a2);
+    }
+
+    @Test
+    public void assert_Not_Null()
+    {
+int i = 5;
+        Assert.assertNotNull(i);
+    }
+}
